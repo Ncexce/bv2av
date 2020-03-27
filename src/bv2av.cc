875ab982b6f2056510978fd30f31694f69b14be7
Ncexce <1825578362@qq.com>
2020-03-27 16:26:07 +0800

    Info Implement

--- src/bv2av.cc
+++ src/bv2av.cc
@@ -1,7 +1,8 @@
-//bv2av, a aid and bvid converter for bilibili
+//bv2av, a av and bv converter for bilibili
 //Code based on mcfx's python code,and algorithm by mcfx
 //Original: https://www.zhihu.com/question/381784377/answer/1099438784
 //This transferred version is still buggy and unusable, and I have no idea why that will happen.
+//encode av -> bv is available now, but decoding bv -> av is still unavailable.
 //pull-requests is STRONGLY WELCOMED if you know how to fix that.
 #include <iostream>
 #include <cmath>
@@ -12,10 +13,12 @@ using namespace std;
 
 //typedefs
 typedef map<char,int> indexer;
+typedef unsigned long long ull;
 
 //function prototypes -- arg1=vxor, arg2=vadd
-string enc(unsigned long long toenc, unsigned long long arg1, unsigned long long arg2);
-unsigned long long dec(string todec,unsigned long long arg1, unsigned long long arg2, indexer dictio);
+// encode av -> bv available now!
+string enc(ull toenc, ull arg1, ull arg2);
+ull dec(string todec,ull arg1, ull arg2);
 
 //global variables
 indexer tr;
@@ -39,43 +42,47 @@ int main(int argc, char *argv[])
 	{
 		tr[table[i]]=i;
 	}
-	unsigned long long vxor=177451812;
-	unsigned long long vadd=8728348608;
-	//unsigned long long vadd=100618342136696320;
+	ull vxor=177451812;
+	ull vadd=8728348608;
+	//ull vadd=100618342136696320;
 	if(ag1=="enc")
 	{
-		unsigned long long param=stoll(ag2);
+		ull param=stoll(ag2);
 		cout<<enc(param,vxor,vadd);
 		return 0;
 	}
 	if(ag1=="dec")
 	{
-		cout<<dec(ag2,vxor,vadd,tr);
+		cout<<"av"<<dec(ag2,vxor,vadd);
 		return 0;
 	}
 
 }
 
-string enc(unsigned long long toenc, unsigned long long arg1, unsigned long long arg2)
+//av->bv is available now!
+string enc(ull toenc, ull arg1, ull arg2)
 {
-	unsigned long long temp1 = (toenc^arg1)+arg2;
+	ull temp1 = (toenc^arg1)+arg2;
 	string tstr="BV1  4 1 7  ";
 	for(int i=0;i<6;i++)
 	{
-		tstr[s[i]]=table[static_cast<unsigned long long>(floor(temp1/(pow(58,i))))%58LL];
-		//cout<<(static_cast<unsigned long long>(floor(temp1/(pow(58,i))))%58)<<endl;
+		tstr[s[i]]=table[static_cast<ull>(floor(temp1/(pow(58,i))))%58ull];
+		//cout<<(static_cast<ull>(floor(temp1/(pow(58,i))))%58ull)<<endl;
 	}
 	return tstr;
 }
 
+//decode bv->av is still not available.
 //annotation: indexing a map: usage: map_name.find(key)->second, returning a value the key holds.
-unsigned long long dec(string todec, unsigned long long arg1, unsigned long long arg2, indexer dictio)
+//annotation: Not necessary. Can be accessed by array directly.
+//annotation: context: typedef unsigned long long ull;
+ull dec(string todec, ull arg1, ull arg2)
 {
-	unsigned long long r=0;
+	ull r=0;
 	for(int i=0;i<6;i++)
 	{
-		r+=(dictio.find(todec[s[i]])->second)*(pow(58,i));
-		cout<<(dictio.find(todec[s[i]])->second)<<endl;
+		r+=(tr[todec[s[i]]])*(pow(58,i));
+		cout<<((tr[todec[s[i]]])*(pow(58,i)))<<endl;
 	}
-	return (r-arg2)^arg1;
+	return ((r-arg2)^arg1);
 }
